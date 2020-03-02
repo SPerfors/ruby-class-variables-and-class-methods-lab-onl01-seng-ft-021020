99ad3916daac0a527b06d62e77dc58cd1598b5e3
Steven Perfors <stevecanbuild@gmail.com>
2020-03-02 22:35:27 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @genre = genre
+    @@genres << genre
+    @artist = artist 
+    @@artists << artist
+    @@count += 1 
+  end
+  
+  def self.count
+    return @@count
+  end
+  
+  def self.artists
+    return @@artists.uniq 
+  end
+  
+  def self.genres 
+    return @@genres.uniq
+  end
+  
+  def self.genre_count 
+  end 
+    
+  
+end
