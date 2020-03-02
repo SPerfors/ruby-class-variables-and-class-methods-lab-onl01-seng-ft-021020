b713d029af3b838a09e003ec0a68f13f944ab162
Steven Perfors <stevecanbuild@gmail.com>
2020-03-02 22:51:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,13 +28,14 @@ class Song
   
   def self.genre_count 
     genre_count = {}
-    genre_count.each do |genre|
+    @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1 
       else 
         genre_count[genre] = 1 
       end
     end
+    genre_count
   end 
   
   
